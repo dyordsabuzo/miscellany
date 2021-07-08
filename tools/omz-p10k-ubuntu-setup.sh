886824eb29de3eb596447329ef126db594c45714
George Abuzo <dyordsabuzo@gmail.com>
2021-07-09 08:53:52 +1000

    Change sh to bash

--- tools/omz-p10k-ubuntu-setup.sh
+++ tools/omz-p10k-ubuntu-setup.sh
@@ -1,12 +1,14 @@
 #!/usr/bin/env bash
 
-echo "Installing zsh"
+echo "Installing apt libraries"
 sudo apt update && sudo apt install -y zsh git
 
-sh -c zsh <<END
+echo "Install Oh My Zsh"
+echo -e "Y" | sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)" <<INSTALL
+    exit
+INSTALL
 
-    echo "Install Oh My Zsh"
-    echo -e "Y" | sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+bash -c zsh <<END
     echo "Install powerlevel10k"
     git clone https://github.com/romkatv/powerlevel10k.git $ZSH_CUSTOM/themes/powerlevel10k
     git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
