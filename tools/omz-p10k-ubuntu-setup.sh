590192a997c6e39cb2966d8ad1e9ead93025bf88
George Abuzo <dyordsabuzo@gmail.com>
2021-07-09 08:52:26 +1000

    Supress interaction when installing omz

--- tools/omz-p10k-ubuntu-setup.sh
+++ tools/omz-p10k-ubuntu-setup.sh
@@ -3,8 +3,10 @@
 echo "Installing zsh"
 sudo apt update && sudo apt install -y zsh git
 
-echo "Install Oh My Zsh"
-sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)" <<END
+sh -c zsh <<END
+
+    echo "Install Oh My Zsh"
+    echo -e "Y" | sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
     echo "Install powerlevel10k"
     git clone https://github.com/romkatv/powerlevel10k.git $ZSH_CUSTOM/themes/powerlevel10k
     git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
