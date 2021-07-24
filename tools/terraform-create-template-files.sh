f6487c857dac652cd8b949436b520fcc38084d6b
Dyords Abuzo <dyordsabuzo@gmail.com>
2021-07-25 09:27:59 +1000

    Add script to create terraform template files

---
+++ tools/terraform-create-template-files.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+echo "Create base terraform templates"
+touch main.tf variables.tf providers.tf
+
+if [ ! -f locals.tf ]
+then
+  echo "Create locals template file"
+  tee locals.tf <<- LOCALS
+  locals {
+    tags = {
+      created_by = "terraform"
+    }
+  }
+LOCALS
+fi
+
+if [ ! -f backend.tf ]
+then
+  echo "Create backend template file"
+  tee backend.tf <<- BACKEND
+  terraform {
+
+  }
+BACKEND
+fi
+
+echo "Create default terraform variable file"
+mkdir -p tfvars && touch tfvars/default.tfvars
