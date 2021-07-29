a4f6b8d82ca3e7487152d04dbd7844fe49deccd2
Dyords Abuzo <dyordsabuzo@gmail.com>
2021-07-29 12:54:23 +1000

    Update with creation of variables and providers tf file

--- tools/terraform-create-template-files.sh
+++ tools/terraform-create-template-files.sh
@@ -3,6 +3,28 @@
 echo "Create base terraform templates"
 touch main.tf variables.tf providers.tf data.tf
 
+if [ ! -f providers.tf ]
+then
+  echo "Create providers file"
+  tee providers.tf <<- PROVIDERS
+  providers "aws" {
+    region = var.region
+  }
+PROVIDERS
+fi
+
+if [ ! -f variables.tf ]
+then
+  echo "Create variables file"
+  tee variables.tf <<- VARIABLES
+  variable "region" {
+    description = "AWS region to create resources in"
+    type  = string
+    default = "ap-southeast-2"
+  }
+VARIABLES
+fi
+
 if [ ! -f locals.tf ]
 then
   echo "Create locals template file"
