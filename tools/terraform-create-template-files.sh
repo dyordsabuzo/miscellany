547c40b76e935c72cc21cd1c3f52888ca5f93bb4
Dyords <dyordsabuzo@gmaail.com>
2021-08-17 19:38:37 +1000

    Update terraform create template

--- tools/terraform-create-template-files.sh
+++ tools/terraform-create-template-files.sh
@@ -1,15 +1,16 @@
 #!/usr/bin/env bash
+set -e
 
 echo "Create base terraform templates"
-touch main.tf variables.tf providers.tf data.tf
+touch main.tf data.tf outputs.tf
 
 if [ ! -f providers.tf ]
 then
   echo "Create providers file"
   tee providers.tf <<- PROVIDERS
-  providers "aws" {
-    region = var.region
-  }
+provider "aws" {
+  region = var.region
+}
 PROVIDERS
 fi
 
@@ -17,11 +18,11 @@ if [ ! -f variables.tf ]
 then
   echo "Create variables file"
   tee variables.tf <<- VARIABLES
-  variable "region" {
-    description = "AWS region to create resources in"
-    type  = string
-    default = "ap-southeast-2"
-  }
+variable "region" {
+  description = "AWS region to create resources in"
+  type  = string
+  default = "ap-southeast-2"
+}
 VARIABLES
 fi
 
@@ -29,11 +30,11 @@ if [ ! -f locals.tf ]
 then
   echo "Create locals template file"
   tee locals.tf <<- LOCALS
-  locals {
-    tags = {
-      created_by = "terraform"
-    }
+locals {
+  tags = {
+    created_by = "terraform"
   }
+}
 LOCALS
 fi
 
@@ -41,11 +42,34 @@ if [ ! -f backend.tf ]
 then
   echo "Create backend template file"
   tee backend.tf <<- BACKEND
-  terraform {
+terraform {
 
-  }
+}
 BACKEND
 fi
 
 echo "Create default terraform variable file"
-mkdir -p tfvars && touch tfvars/default.tfvars
+mkdir -p tfvars && touch tfvars/main.tfvars
+
+echo "Create terraform prehook"
+mkdir -p hooks
+
+tee hooks/pre-commit <<- PRECOMMIT
+#!/bin/sh -e
+
+base_path=`pwd`
+processed_paths=()
+
+for changed_file in `git diff --name-only`
+do
+  changed_path=`dirname $changed_file`
+  if [[ ! ${processed_paths[*]} =~ $changed_path ]]
+  then
+    cd $base_path/$changed_path && terraform fmt -recursive && \
+      terraform init -backend=false && terraform validate
+
+    processed_paths+=($changed_path)
+  fi
+done
+PRECOMMIT
+chmod +x hooks/pre-commit
