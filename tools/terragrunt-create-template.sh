6f2462f31cbd1c439a679f902fafd7dcdb2eea93
gabuzo-scw <gabuzo@securecodewarrior.com>
2022-01-19 10:36:03 +1100

    Add terragrunt script

---
+++ tools/terragrunt-create-template.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+set -e
+
+file_locations=$(find . -type f \( -name "variables.tf" \))
+
+for f in $file_locations
+do
+    input_block=$(awk '/variable/ && gsub(/\"/,"") {printf "  %s = \"\"\n", $2}' $f)
+
+    config=$(cat <<EOL
+include {
+  path = find_in_parent_folders()
+}
+
+terraform {
+  source = "."
+}
+
+dependency {}
+
+inputs = {
+$input_block
+}
+EOL
+)
+
+    echo -e "$config" > $(dirname $f)/terragrunt.hcl
+done
+
+echo -e "locals {\n}" > terragrunt.hcl
