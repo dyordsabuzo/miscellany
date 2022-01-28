37ac7b6ccefa27f5cd72e896bc0277130399fa7c
George Abuzo <dyordsabuzo@gmail.com>
2022-01-28 15:52:30 +1100

    Fix faling problems when creating multiple workspaces

--- tools/terraform-create-all-workspaces.sh
+++ tools/terraform-create-all-workspaces.sh
@@ -5,7 +5,6 @@ file_locations=$(find . -maxdepth 2 -type f \( -name "backend.tf" \))
 
 for f in $file_locations
 do
-    TF_MODULE_PATH=$(dirname $f) sh -c \
-        "$(curl -fsSL https://raw.githubusercontent.com/dyordsabuzo/miscellany/8237af077493b63378d59efc4a8d9ea3859cc848/tools/terraform-workspace-local.sh)"
-        # "$(curl -fsSL https://raw.githubusercontent.com/dyordsabuzo/miscellany/main/tools/terraform-workspace-local.sh)"
+    TF_MODULE_PATH=$(dirname $f) bash -c \
+        "$(curl -fsSL https://raw.githubusercontent.com/dyordsabuzo/miscellany/main/tools/terraform-workspace-local.sh)"
 done
